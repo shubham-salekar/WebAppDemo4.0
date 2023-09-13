2c0a82f495f37fbbe6702a95f919a975ac2e401e
Shardeen1 <sanjeev@shardeen.com>
2023-09-13 18:17:01 +0530

    validations

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace WebAppDemo4._0.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+        [MaxLength(50,ErrorMessage ="Name limit exeeded")]
+        [Required]
         public string Name { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",ErrorMessage ="invalid email format")]
+        [Required]
+        [Display(Name ="Office Mail",Prompt ="select")]
         public string Email { get; set; }
-        public Dept Department { get; set;}
-        //Console.WriteLine("Employee");
+        [Required]
+        public Dept? Department { get; set;}
         
 
     }
