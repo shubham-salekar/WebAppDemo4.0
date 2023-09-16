791e2437224e3994095be60b98bc880d68815e0a
Shardeen1 <sanjeev@shardeen.com>
2023-09-16 17:53:38 +0530

    file upload and adding multiple files

---
+++ ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using WebAppDemo4._0.Models;
+
+namespace WebAppDemo4._0.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        [MaxLength(50, ErrorMessage = "Name limit exeeded")]
+        [Required]
+        public string Name { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "invalid email format")]
+        [Required]
+        [Display(Name = "Office Mail", Prompt = "select")]
+        public string Email { get; set; }
+        [Required]
+        public Dept? Department { get; set; }
+        public List<IFormFile> Photos { get; set; }
+    }
+}
