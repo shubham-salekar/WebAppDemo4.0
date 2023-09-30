75ee65218e2e7993cc019e0a0f1c1d6a7dafdd50
sudarshan <sudarshan@shardeen.com>
2023-09-30 17:01:42 +0530

    commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppDemo4._0.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
